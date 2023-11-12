4fe35b06bc9474a9e451cea9d10ace06db694621
alexanderHaskins <alexjhaskins@yahoo.com>
2023-11-11 19:40:23 -0600

    Started working on the Instructor web page. Got the GridView to display the requested data. Still need to get it to display first and last name of the instructor.

---
+++ Instructor.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSCI213Assignment4
+{
+    public partial class Instructor1 : System.Web.UI.Page
+    {
+        KarateSchoolDataContext dbcon;
+        string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\alexj\\OneDrive\\Desktop\\HaskinsAlexanderAssignment4\\CSCI213Assignment4\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dbcon=new KarateSchoolDataContext(conn);
+            var result=from x in dbcon.Sections
+                       join  y in dbcon.Members
+                       on x.Member_ID equals y.Member_UserID
+                       select new {
+                           x.SectionName,
+                           y.MemberFirstName,
+                           y.MemberLastName
+                       };
+            sectionAndMembersGridView.DataSource = result;
+            sectionAndMembersGridView.DataBind();
+        }
+    }
+}
