51e4d724ee88f849e79c01fd3c7def30560652e5
alexanderHaskins <alexjhaskins@yahoo.com>
2023-11-17 17:05:36 -0600

    Tried to add the functionality to have the page display the user's first and last name, ran into an error that prevents the code from running

--- Instructor.aspx.cs
+++ Instructor.aspx.cs
@@ -11,9 +11,25 @@ namespace CSCI213Assignment4
     {
         KarateSchoolDataContext dbcon;
         string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\alexj\\OneDrive\\Desktop\\HaskinsAlexanderAssignment4\\CSCI213Assignment4\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
+
+        
         protected void Page_Load(object sender, EventArgs e)
         {
             dbcon=new KarateSchoolDataContext(conn);
+
+            // string myUserName=User.Identity.Name;
+            string myUserName = "user2";
+            NetUser myKRMember = (from x in dbcon.NetUsers
+                                  where x.UserName == myUserName
+                                  select x).First();
+            int myUserId = myKRMember.UserID;
+
+            Instructor myInstructor =(from x in dbcon.Instructors
+                                      where x.InstructorID==myUserId
+                                      select x).First();
+            firstNameLabel.Text = myInstructor.InstructorFirstName;
+            lastNameLabel.Text = myInstructor.InstructorLastName;
+
             var result=from x in dbcon.Sections
                        join  y in dbcon.Members
                        on x.Member_ID equals y.Member_UserID
